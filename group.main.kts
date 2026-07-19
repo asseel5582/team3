c8d2504de26d296bdaf75ca1f04d39d0682f7f13
saif-almograbe <120220622154@st.ahu.edu.jo>
2026-07-19 15:44:11 +0300

    hi team

--- group.main.kts
+++ group.main.kts
@@ -1,6 +1,6 @@
 fun main() {
     println("Leader = Asseel")
-    println("Team Member 1 = ")
+    println("Team Member 1 =saif ")
     println("Team Member 2 = ")
     println("Team Member 3 = ")
     println("Team Member 4 = ")
