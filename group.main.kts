cdb227d831b2fc2a415b80c170bf5044dc8f362f
Abdalrahman <abdalrahmandaraghmeh05@gmail.com>
2026-07-19 16:01:14 +0300

    hey again

--- group.main.kts
+++ group.main.kts
@@ -1,7 +1,7 @@
 fun main() {
     println("Leader = Asseel")
     println("Team Member 1 = ")
-    println("Team Member 2 = abdalrahman")
+    println("Team Member 2 = abdalrahman ")
     println("Team Member 3 = ")
     println("Team Member 4 = ")
 }
