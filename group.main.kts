c8b1468ac55bfb7f34da2411f1568b1c56cb26f1
mohammad <mohammadamjad9066@gmail.com>
2026-07-19 15:42:52 +0300

    ok

--- group.main.kts
+++ group.main.kts
@@ -1,6 +1,6 @@
 fun main() {
     println("Leader = Asseel")
-    println("Team Member 1 = ")
+    println("Team Member 1 =fares ")
     println("Team Member 2 = ")
     println("Team Member 3 = ")
     println("Team Member 4 = ")
